ea49500af228ce3cafd00cdf9102ac302c57e32e
gitaa001 <18223122@std.stei.itb.ac.id>
2025-10-11 12:06:46 +0700

    refactor user schema

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,34 @@
 import mongoose from "mongoose";
 
+// SKEMA PRACTICE
+const progressPracticeSchema = new mongoose.Schema({
+  bubbleSort: { type: Boolean, default: false },
+  selectionSort: { type: Boolean, default: false },
+  insertionSort: { type: Boolean, default: false },
+  mergeSort: { type: Boolean, default: false },
+});
+
+// SKEMA COMPETE 
+const progressCompeteSchema = new mongoose.Schema({
+  bubbleSort: {
+    score: { type: Number, default: null },
+    done: { type: Boolean, default: false },
+  },
+  selectionSort: {
+    score: { type: Number, default: null },
+    done: { type: Boolean, default: false },
+  },
+  insertionSort: {
+    score: { type: Number, default: null },
+    done: { type: Boolean, default: false },
+  },
+  mergeSort: {
+    score: { type: Number, default: null },
+    done: { type: Boolean, default: false },
+  },
+});
+
+// SKEMA USER
 const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
@@ -7,33 +36,8 @@ const userSchema = new mongoose.Schema(
     password: { type: String, required: true },
     totalPoints: { type: Number, default: 0 },
 
-    // PROGRESS BAGIAN PRACTICE
-    progressPractice: {
-      bubbleSort: { type: Boolean, default: false },
-      selectionSort: { type: Boolean, default: false },
-      insertionSort: { type: Boolean, default: false },
-      mergeSort: { type: Boolean, default: false },
-    },
-
-    // PROGRESS BAGIAN COMPETE 
-    progressCompete: {
-      bubbleSort: {
-        score: { type: Number, default: null },
-        done: { type: Boolean, default: false },
-      },
-      selectionSort: {
-        score: { type: Number, default: null },
-        done: { type: Boolean, default: false },
-      },
-      insertionSort: {
-        score: { type: Number, default: null },
-        done: { type: Boolean, default: false },
-      },
-      mergeSort: {
-        score: { type: Number, default: null },
-        done: { type: Boolean, default: false },
-      },
-    },
+    progressPractice: { type: progressPracticeSchema, default: () => ({}) },
+    progressCompete: { type: progressCompeteSchema, default: () => ({}) },
   },
   { timestamps: true }
 );
